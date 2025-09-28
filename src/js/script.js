baf1cde17d0ce155dd2f747286ea2b72dfa3c94c
ShivamSharma95 <30391874+ShivamSharma95@users.noreply.github.com>
2025-09-28 15:39:27 +0000

    new design

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,10 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
   alert('Thanks for your message!');
   this.reset();
 });
+// Simple smooth scroll for navbar links
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
